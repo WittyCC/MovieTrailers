81ced8f6d1669954aa319fb32da2a11359656659
Witty Chang <wcc1213@gmail.com>
2017-10-05 05:50:56 -0700

    Add functionality to MainActivity and integrate YouTube API

---
+++ app/src/main/java/com/epicodus/movietrailers/MainActivity.java
@@ -0,0 +1,56 @@
+package com.epicodus.movietrailers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends YouTubeBaseActivity {
+
+    private static final String TAG = "MainActivity";
+
+    YouTubePlayerView mYouTubePlayerView;
+    Button buttonPlay;
+    YouTubePlayer.OnInitializedListener mOnInitializedListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: Starting.");
+
+        buttonPlay = (Button) findViewById(R.id.buttonPlay);
+        mYouTubePlayerView = (YouTubePlayerView) findViewById(R.id.youTubePlay);
+        mOnInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                Log.d(TAG, "onClick: Done initializing.");
+                youTubePlayer.loadPlaylist("PLelK7aEMtfK2wpv7tv472alrDpNwO_0jw");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.d(TAG, "onClick: Failed to initialize.");
+
+            }
+        };
+
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Initializing YouTube Player.");
+                mYouTubePlayerView.initialize(YouTubeConfig.getApiKey(), mOnInitializedListener);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/epicodus/movietrailers/YouTubeConfig.java
@@ -0,0 +1,12 @@
+package com.epicodus.movietrailers;
+
+public class YouTubeConfig {
+    public YouTubeConfig() {
+    }
+
+    private static final String API_KEY = "AIzaSyD0vqbRn1Uw2fGRzm2FEynrZOrpfP9Un4w";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
